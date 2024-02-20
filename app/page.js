b6ebb6377e2804d6eb60cd23fbbf5a130ed30d6e
Guney Unay <guneymarketing17@hotmail.com>
2024-02-20 18:05:11 +0300

    update page.js

--- app/page.js
+++ app/page.js
@@ -1,3 +1,10 @@
+import Button from "@/components/Button";
+
 export default function Home() {
-  return <main>test</main>;
+  return (
+    <main className="bg-ebony-clay w-screen h-screen">
+      <Button variant="primary" title="deneme" />
+      <Button variant="secondary" title="deneme" />
+    </main>
+  );
 }
