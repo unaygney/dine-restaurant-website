2f2637de9b3cd492c7d4239b915c2e0c26b28eba
Guney Unay <guneymarketing17@hotmail.com>
2024-02-20 18:07:40 +0300

    added colors in config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,12 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      beaver: "#9e7f66",
+      "cod-gray": "#111111",
+      mirage: "#17192b",
+      "ebony-clay": "#242b37",
+      "shuttle-gray": "#5c6779",
     },
   },
   plugins: [],
