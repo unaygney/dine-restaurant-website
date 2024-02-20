e4a4b5241ce27b03e5725fd871cc76da4b565a9c
Guney Unay <guneymarketing17@hotmail.com>
2024-02-20 16:09:01 +0300

    added custom font

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,5 @@
-import { Inter } from "next/font/google";
+import { mainFont } from "@/lib/font";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Dine Restaurant Website",
@@ -11,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={mainFont.className}>{children}</body>
     </html>
   );
 }
