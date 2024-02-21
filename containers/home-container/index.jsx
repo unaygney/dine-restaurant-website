7a344fc39efe23373a02b158b80a709bf170abcb
Guney Unay <guneymarketing17@hotmail.com>
2024-02-21 20:42:32 +0300

    added footer component

--- containers/home-container/index.jsx
+++ containers/home-container/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import HomeHero from "./home-hero";
-
+import Footer from "@/components/Footer";
 export default function HomeContainer() {
   return (
     <main>
       <HomeHero />
+      <Footer />
     </main>
   );
 }
