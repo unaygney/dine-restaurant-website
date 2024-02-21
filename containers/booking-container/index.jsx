cbd0a77792ab59353989fb425d370feb42392c4c
Guney Unay <guneymarketing17@hotmail.com>
2024-02-21 20:42:15 +0300

    added footer component

--- containers/booking-container/index.jsx
+++ containers/booking-container/index.jsx
@@ -1,5 +1,10 @@
+import Footer from "@/components/Footer";
 import React from "react";
 
 export default function BookingContainer() {
-  return <div>booking container</div>;
+  return (
+    <main>
+      <Footer />
+    </main>
+  );
 }
