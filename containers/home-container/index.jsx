a132e1769dcac3ac4e427ac97c7481ecc65b7b60
Guney Unay <guneymarketing17@hotmail.com>
2024-02-23 17:46:35 +0300

    added highligtscard component in homecontainer

--- containers/home-container/index.jsx
+++ containers/home-container/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HomeHero from "./home-hero";
 import RestaurantCard from "./restourant-card";
+import HighlightsCard from "./highligts-card";
 import Footer from "@/components/Footer";
 export default function HomeContainer() {
   return (
     <main>
       <HomeHero />
       <RestaurantCard />
+      <HighlightsCard />
       <Footer />
     </main>
   );
