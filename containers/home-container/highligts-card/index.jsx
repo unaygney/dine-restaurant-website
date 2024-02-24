432b3557904ff3dd271cc11e7b650bc68942492b
Guney Unay <guneymarketing17@hotmail.com>
2024-02-24 15:38:51 +0300

    update higligtcard componnet

--- containers/home-container/highligts-card/index.jsx
+++ containers/home-container/highligts-card/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function HighlightsCard() {
-  return <section>HighlightsCard</section>;
+  return <section>HighlightsCard item</section>;
 }
 
 export default HighlightsCard;
