f0022307d3d742c408c261a362a677522af237c7
Guney Unay <guneymarketing17@hotmail.com>
2024-02-26 20:27:35 +0300

    added tabs-card

--- containers/home-container/index.jsx
+++ containers/home-container/index.jsx
@@ -4,12 +4,14 @@ import RestaurantCard from "./restourant-card";
 import HighlightsCard from "./highligts-card";
 import Footer from "@/components/Footer";
 import ReservationCard from "@/components/ReservationCard";
+import TabsCard from "./tabs-card";
 export default function HomeContainer() {
   return (
     <main>
       <HomeHero />
       <RestaurantCard />
       <HighlightsCard />
+      <TabsCard />
       <ReservationCard />
       <Footer />
     </main>
